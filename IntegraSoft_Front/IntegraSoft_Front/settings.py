fb2a0a28cd1cd680bb9049be0fb5734a42c5dc7e
Juan-CCU <jlavado@ccu.cl>
2023-08-22 11:30:36 -0400

    commit temporal

--- IntegraSoft_Front/IntegraSoft_Front/settings.py
+++ IntegraSoft_Front/IntegraSoft_Front/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'IntegraSoft_Front',
+    'cuenta',
     'modelos',
     'templates_generales',
     'mantenedor_works',
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
@@ -120,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- IntegraSoft_Front/IntegraSoft_Front/urls.py
+++ IntegraSoft_Front/IntegraSoft_Front/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('works/', include('mantenedor_works.urls') ),
+    path("cuenta/", include('cuenta.urls')),
     path('', include('templates_generales.urls')),
 ]
