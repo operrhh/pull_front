fa2a638ada999cb52cfe5a17a8b4f2cecb92904b
Juan-CCU <jlavado@ccu.cl>
2023-10-17 10:43:26 -0400

    archivos actualizados desde server

--- IntegraSoft_Front/IntegraSoft_Front/settings.py
+++ IntegraSoft_Front/IntegraSoft_Front/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.urls import reverse_lazy 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0r9bv5yqum2lpvl8bb24wlw_-2f0nma67(30df_r3w1^%r3z%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.104','0.0.0.0','127.0.0.1']
 
 
 # Application definition
@@ -124,8 +124,8 @@ LOGOUT_REDIRECT_URL = reverse_lazy('login')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
