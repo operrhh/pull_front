52076e4a7131083fb18b824f74175c2d34cfe8a7
Juan-CCU <142257499+Juan-CCU@users.noreply.github.com>
2023-10-18 11:35:18 -0300

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Ejecutar comandos en el servidor remoto
-ssh -o StrictHostKeyChecking=no ${{ secrets.SERVER_USERNAME }}@${{ secrets.SERVER_IP }} << 'ENDSSH'
+ssh -o StrictHostKeyChecking=no $SERVER_USERNAME@$SERVER_IP << 'ENDSSH'
   cd /home/integrasoft/front/pull_front/IntegraSoft_Front
   git pull origin main
 ENDSSH
+
