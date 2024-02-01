cd9d39bbf8277c0f212261f890d42db7e69bfee8
Juan-CCU <jlavado@ccu.cl>
2024-02-01 19:41:00 -0300

    oci

--- IntegraSoft_Front/IntegraSoft_Front/settings.py
+++ IntegraSoft_Front/IntegraSoft_Front/settings.py
@@ -20,7 +20,7 @@ API_BASE_URL = config('API_BASE_URL')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+# OCI
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
